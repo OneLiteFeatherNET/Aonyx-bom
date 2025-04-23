76f879c785f6f829558fd4bd189f963ffe010f93
theEvilReaper <steffenwx@gmail.com>
2025-04-23 11:01:38 +0200

    Add project files

---
+++ build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+    `maven-publish`
+    `java-platform`
+}
+
+group = "net.onelitefeather.aonyx.bom"
+version = "1.0.0"
+
+javaPlatform {
+    allowDependencies()
+}
+
+dependencies {
+    constraints {
+        api(platform(libs.mycelium.bom))
+        api(libs.aves)
+        api(libs.xerus)
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            authentication {
+                credentials(PasswordCredentials::class) {
+                    // Those credentials need to be set under "Settings -> Secrets -> Actions" in your repository
+                    username = System.getenv("ONELITEFEATHER_MAVEN_USERNAME")
+                    password = System.getenv("ONELITEFEATHER_MAVEN_PASSWORD")
+                }
+            }
+            name = "OneLiteFeatherRepository"
+            url = if (project.version.toString().contains("SNAPSHOT")) {
+                uri("https://repo.onelitefeather.dev/onelitefeather-snapshots")
+            } else {
+                uri("https://repo.onelitefeather.dev/onelitefeather-releases")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["javaPlatform"])
+            pom {
+                name.set("${project.name} ${project.version}")
+                description.set("Bill of materials for the Aonyx project")
+                developers {
+                    developer {
+                        name.set("OneliteFeather")
+                        contributors {
+                            contributor {
+                                name.set("theEvilReaper")
+                            }
+                            contributor {
+                                name.set("TheMeinerLP")
+                            }
+                        }
+                    }
+                }
+
+                issueManagement {
+                    system.set("Github")
+                    url.set("https://github.com/OneLiteFeatherNET/Aonyx-bom/issues")
+                }
+            }
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,34 @@
+rootProject.name = "aonyx-bom"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        maven {
+            name = "OneLiteFeatherRepository"
+            url = uri("https://repo.onelitefeather.dev/onelitefeather")
+            if (System.getenv("CI") != null) {
+                credentials {
+                    username = System.getenv("ONELITEFEATHER_MAVEN_USERNAME")
+                    password = System.getenv("ONELITEFEATHER_MAVEN_PASSWORD")
+                }
+            } else {
+                credentials(PasswordCredentials::class)
+                authentication {
+                    create<BasicAuthentication>("basic")
+                }
+            }
+        }
+    }
+    versionCatalogs {
+        create("libs") {
+            version("aves", "1.6.1")
+            version("xerus", "1.3.0")
+            version("shadow", "8.3.6")
+            version("mycelium.bom", "1.1.2")
+
+            library("mycelium.bom", "net.theevilreaper.mycelium.bom", "mycelium-bom").versionRef("mycelium.bom")
+            library("aves", "de.icevizion.lib", "aves").versionRef("aves")
+            library("xerus", "net.theevilreaper.xerus", "xerus").versionRef("xerus")
+        }
+    }
+}

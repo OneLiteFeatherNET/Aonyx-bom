5a2ba37d7ce28e2dde399ad710d02cd8cf5ea895
Steffen Wonning <6805083+theEvilReaper@users.noreply.github.com>
2025-12-24 12:19:20 +0100

    Update library versions in settings.gradle.kts
    Include version bumps for Aves, Xerus and Guira

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,10 +21,10 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            version("aves", "1.12.0")
-            version("xerus", "1.9.0")
+            version("aves", "1.13.0")
+            version("xerus", "1.10.0")
             version("mycelium.bom", "1.6.0")
-            version("guira", "0.7.0")
+            version("guira", "0.8.0")
 
             library("mycelium.bom", "net.onelitefeather", "mycelium-bom").versionRef("mycelium.bom")
             library("guira", "net.onelitefeather", "guira").versionRef("guira")
